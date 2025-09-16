8c0fa0d42f18a3aab7cae80760ed497da65bb259
alex-deev <ax.onestar@gmail.com>
2025-09-16 19:45:40 -0300

    feat: add stateless user sessions with JWT
    also adds protected route /protected with authentication required.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT ?? 3000;
 
 export const SALT_ROUNDS = process.env.SALT_ROUNDS ?? 10;
+
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
-import { PORT } from './config.js';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET_KEY, PORT } from './config.js';
 import { UserRepository } from './repository/user-repository.js';
 
 const app = express();
+
 app.set('view engine', 'ejs');
-app.use(express.json()); // Use middleware to transform req.body to JSON
+app.use(express.json()); // Middleware to transform req.body to JSON
+app.use(cookieParser()); // Middleware to attach/modify Cookies on requests
 
 app.use((req, res, next) => {
 	const timestamp = new Date().toISOString();
@@ -13,15 +17,39 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-	res.render('./index', { name: 'alex' });
+	res.render('./index');
 });
 
 // User session control
 app.post('/login', async (req, res) => {
+	const { username, password } = req.body;
 	try {
-		const { username, password } = req.body;
+		// 1. retrieves user data from DB
 		const user = await UserRepository.login({ username, password });
-		res.send(user);
+
+		// 2. creates Json Web Token Signature
+		const token = await jwt.sign(
+			{
+				// payload
+				id: user._id,
+				username: username,
+			},
+			JWT_SECRET_KEY, // signature key
+			{
+				// header meta
+				expiresIn: '1h',
+			},
+		);
+
+		// 3. responds with cookie and data
+		res
+			.cookie('access_token', token, {
+				httpOnly: true, // only server management of cookie
+				sameSite: 'strict', // cookie only available on same domain
+				maxAge: 1000 * 60 * 60, // cookie expires after 1 hour
+				// secure: true, // DISABLED cookie only accessible via HTTPs
+			})
+			.send(user);
 	} catch (error) {
 		res.status(401).send(error.message);
 	}
@@ -38,7 +66,25 @@ app.post('/register', async (req, res) => {
 app.post('/logout', (req, res) => {});
 
 // Protected route
-app.post('/protected', (req, res) => {});
+app.get('/protected', (req, res) => {
+	// 1. checks if token is present (means user previously authenticated)
+	const token = req.cookies.access_token;
+	if (!token) {
+		return res.status(403).send('Cannot access here. Protected route 🔒.');
+	}
+
+	try {
+		// 2. checks if token is valid for this site (has the same header, payload and signature key)
+		const data = jwt.verify(token, JWT_SECRET_KEY);
+
+		// 3. authorizes the request and provide the route source
+		res.render('./protected', data);
+	} catch (error) {
+		return res
+			.status(401)
+			.send('Access not authorized. Authentication is not valid.');
+	}
+});
 
 const server = app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}...`);
